75ea7b893bb6cd28c5cce487fe893af9bc3d1588
Grace Mugoiri <gracemugoiri@gmail.com>
2026-04-09 16:51:54 +0300

    Initial skeleton commit

---
+++ app.py
@@ -0,0 +1,9 @@
+# lets create a simple flask app
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
+    
